ac80a5ee99354368977492a26fd7784ce0227122
hc <hc@email.com>
2018-01-22 11:24:20 +0800

    启动页

--- pages/start/start.js
+++ pages/start/start.js
@@ -10,11 +10,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    setTimeout(function () {
-      wx.reLaunch({
-        url: '../merchant/merchant'
-      })
-    }, 1000)
+    
   },
 
   /**
@@ -28,7 +24,11 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+    setTimeout(function () {
+      wx.reLaunch({
+        url: '../merchant/merchant'
+      })
+    }, 1000)
   },
 
   /**
